ba7244350eb02a33d9c356b9dda666c8d86bca42
hannes keunen <hannes.keunen@student.uhasselt.be>
2019-02-28 11:19:49 +0100

    Added c++ glue code for GRALE

--- build-grale.sh
+++ build-grale.sh
@@ -1,1 +1,1 @@
-emcc cpp/grale_glue.cpp wasm/libgrale2core.a -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -o wasm/grale.js -I/usr/local/include
+emcc cpp/grale_glue.cpp bin/libgrale2core.a -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -s -o bin/grale.js -I/usr/local/include
--- cpp/grale_glue.cpp
+++ cpp/grale_glue.cpp
@@ -1,12 +1,72 @@
-#include <grale/circularsource.h>
+#include <grale/plummerlens.h>
+#include <grale/masssheetlens.h>
+#include <grale/nsislens.h>
+#include <grale/nsielens.h>
+#include <grale/sielens.h>
+#include <grale/sislens.h>
+
 #include <emscripten/emscripten.h>
+
+#include <iostream>
 
 extern "C" {
 
-const char *EMSCRIPTEN_KEEPALIVE test() {
-    grale::Vector2Dd vec;
-    grale::CircularSource source(vec, 0, 0);
-    return source.getObjectName().c_str();
+grale::PlummerLens *EMSCRIPTEN_KEEPALIVE createPlummerLens(double D_d, double mass, double angularWidth) {
+    grale::PlummerLens *lens = new grale::PlummerLens;
+    grale::PlummerLensParams params(mass, angularWidth);
+    lens->init(D_d, &params);
+    return lens;
+}
+
+grale::SISLens *EMSCRIPTEN_KEEPALIVE createSISLens(double D_d, double velocityDispersion) {
+    grale::SISLens *lens = new grale::SISLens;
+    grale::SISLensParams params(velocityDispersion);
+    lens->init(D_d, &params);
+    return lens;
+}
+
+grale::NSISLens *EMSCRIPTEN_KEEPALIVE createNSISLens(double D_d, double velocityDispersion, double angularCoreRadius) {
+    grale::NSISLens *lens = new grale::NSISLens;
+    grale::NSISLensParams params(velocityDispersion, angularCoreRadius);
+    lens->init(D_d, &params);
+    return lens;
+}
+
+grale::SIELens *EMSCRIPTEN_KEEPALIVE createSIELens(double D_d, double velocityDispersion, double ellipticity) {
+    grale::SIELens *lens = new grale::SIELens;
+    grale::SIELensParams params(velocityDispersion, ellipticity);
+    lens->init(D_d, &params);
+    return lens;
+}
+
+grale::NSIELens *EMSCRIPTEN_KEEPALIVE createNSIELens(double D_d, double velocityDispersion, double ellipticity, double angularCoreRadius) {
+    grale::NSIELens *lens = new grale::NSIELens;
+    grale::NSIELensParams params(velocityDispersion, ellipticity, angularCoreRadius);
+    lens->init(D_d, &params);
+    return lens;
+}
+
+grale::MassSheetLens *EMSCRIPTEN_KEEPALIVE createMassSheetLens(double D_d, double density) {
+    grale::MassSheetLens *lens = new grale::SIELens;
+    grale::MassSheetLensParams params(density);
+    lens->init(D_d, &params);
+    return lens;
+}
+
+void EMSCRIPTEN_KEEPALIVE calculateLensAlphaX(grale::GravitationalLens lens, double D_d, double D_ds, double theta_x, double theta_y) {
+    grale::Vector2Dd alpha;
+    lens->getAlphaVector(grale::Vector2Dd(theta_x, theta_y), &alpha);
+    return alpha.x;
+}
+
+void EMSCRIPTEN_KEEPALIVE calculateLensAlphaY(grale::GravitationalLens lens, double D_d, double D_ds, double theta_x, double theta_y) {
+    grale::Vector2Dd alpha;
+    lens->getAlphaVector(grale::Vector2Dd(theta_x, theta_y), &alpha);
+    return alpha.y;
+}
+
+void EMSCRIPTEN_KEEPALIVE destroyLens(grale::GravitationalLens *lens) {
+    delete lens;
 }
 
 }
