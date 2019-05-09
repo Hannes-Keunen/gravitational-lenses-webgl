4f3ca44b4b5a1ffaf2b0a4ddff413ae93f28c3fb
hannes keunen <hannes.keunen@student.uhasselt.be>
2019-05-09 21:53:08 +0200

    Caustics bugfix

--- build-grale.sh
+++ build-grale.sh
@@ -8,8 +8,8 @@ emcc \
     -s WASM=1 \
     -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
     -s ASSERTIONS=2 \
-    -s ALLOW_MEMORY_GROWTH=1 \
     -s FORCE_FILESYSTEM=1 \
     -s SAFE_HEAP=1 \
     -o bin/grale.js \
     -I/usr/local/include
+    # -s ALLOW_MEMORY_GROWTH=1 \
